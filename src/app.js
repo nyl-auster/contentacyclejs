7b2f5ab50ace6591a73a8e2c3261ee20dd001fbb
yann <yann.boisselier@gmail.com>
2017-07-17 23:09:52 +0200

    trying cycle.js

---
+++ src/app.js
@@ -0,0 +1,32 @@
+import { div, h1, h2, p, label, ul, li, input, hr, a } from '@cycle/dom'
+import xs from 'xstream'
+
+export function App (sources) {
+
+  let recipesRequest$ = xs.of({
+    url: 'https://dev-contentacms.pantheonsite.io/api/recipes', // GET method by default
+    category: 'recipes',
+  });
+
+  let response$ = sources.HTTP
+    .select('recipes')
+    .flatten()
+    .map(response => response.body.data)
+
+  let vdom$ = response$
+    .startWith([])
+    .map(results =>
+      div('.recipes-list', results.map(result =>
+        div('.recipe', [
+          h2(result.attributes.title),
+          p(result.attributes.ingredients)
+        ])
+      ))  
+    )
+
+  const sinks = {
+    DOM: vdom$,
+    HTTP: recipesRequest$
+  }
+  return sinks
+}
